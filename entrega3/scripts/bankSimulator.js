76b0eb1363580a861838a890d2ecff7f8c31ae3c
tinchods93 <mds.designss@gmail.com>
2025-01-11 13:45:51 -0300

    feat: usd handling and alerts replaced by sweetAlert

--- entrega3/scripts/bankSimulator.js
+++ entrega3/scripts/bankSimulator.js
@@ -60,6 +60,16 @@ const deleteItem = (id, table) => {
   return true;
 };
 
+// resuelve la ruta relativa dentro de "pages"
+const pathResolver = (path) => {
+  const pathList = window.location.pathname.split('/');
+  const pagesIndex = pathList.indexOf('pages');
+  path.split('/').forEach((pathPart, index) => {
+    pathList[pagesIndex + index + 1] = pathPart;
+  });
+  window.location.pathname = pathList.join('/');
+};
+
 // Funciones de sesión
 let userSession = null;
 
@@ -134,7 +144,7 @@ const setAuthListeners = () => {
 };
 
 // Registra un nuevo usuario
-const registerUser = (event) => {
+const registerUser = async (event) => {
   event.preventDefault();
   const usernameValue = usernameInput.value;
   const passwordValue = passwordInput.value;
@@ -151,19 +161,36 @@ const registerUser = (event) => {
     passwordErrorMessage.innerText = 'Las contraseñas no coinciden';
     return;
   }
-  const newUser = { id: usernameValue, password: passwordValue, balance: 0 };
+  const newUser = {
+    id: usernameValue,
+    password: passwordValue,
+    balance: {
+      ars: 0,
+      usd: 0,
+    },
+  };
   const user = getItemById(usernameValue, 'users');
   if (user) {
     nameErrorMessage.innerText = 'El usuario ya existe';
     return;
   }
   addItem(newUser, 'users');
-  alert('Usuario registrado correctamente');
-  pathResolver('auth/login.html');
+
+  const swalResult = await Swal.fire({
+    title: 'Registro exitoso',
+    icon: 'success',
+    confirmButtonText: 'Cool',
+    timer: 2000,
+    timerProgressBar: true,
+  });
+
+  if (swalResult.isConfirmed || swalResult.isDismissed) {
+    pathResolver('auth/login.html');
+  }
 };
 
 // Inicia sesión de usuario
-const loginUser = (event) => {
+const loginUser = async (event) => {
   event.preventDefault();
   const usernameValue = usernameInput.value;
   const passwordValue = passwordInput.value;
@@ -185,8 +212,18 @@ const loginUser = (event) => {
     return;
   }
   addSession(user);
-  alert('Usuario logueado correctamente');
-  pathResolver('account/account.html');
+
+  const swalResult = await Swal.fire({
+    title: 'Ingreso exitoso',
+    icon: 'success',
+    confirmButtonText: 'Cool',
+    timer: 2000,
+    timerProgressBar: true,
+  });
+
+  if (swalResult.isConfirmed || swalResult.isDismissed) {
+    pathResolver('account/account.html');
+  }
 };
 
 // Cierra sesión de usuario
@@ -199,18 +236,17 @@ const logoutUser = () => {
 // Funciones de movimientos
 const recipientComponentModel = `<span class='movement-recipient'>:recipient</span>`;
 const movementComponentModel = `<li>
-    <div class="movement-up">
-      <div class='movement-left'>
+    <div class="movement">
+      <div class="movement-left">
         :recipient-component
-        <span class='movement-sender'>:sender</span>
-        <span class='movement-type'>:type</span>
+        <span class="movement-sender">:sender</span>
+        <span class="movement-type">:type</span>
+        <span class="movement-date">:date</span>
       </div>
-      <div class='movement-right'>
-        <span class='movement-amount'>:amount</span>
+      <div class="movement-right">
+        <span class="movement-amount">:amount</span>
+        <span class="movement-currency">:currency</span>
       </div>
-    </div>
-    <div class="movement-down">
-      <span class='movement-date'>:date</span>
     </div>
   </li>`;
 
@@ -248,6 +284,8 @@ const buildMovementComponent = (movement) => {
         ':recipient-component',
         recipientComponent
       );
+      // si es una transferencia hacia otro, el monto es negativo
+      movement.amount = -movement.amount;
     } else {
       movementComponent = movementComponent.replace(':recipient-component', '');
     }
@@ -265,6 +303,11 @@ const buildMovementComponent = (movement) => {
     movementComponent = movementComponent.replace(':sender', '');
   }
 
+  // si es un retiro o una compra de dolares, el monto es negativo
+  if (['retiro', 'compra usd'].includes(movement.movement_type)) {
+    movement.amount = -movement.amount;
+  }
+
   movementComponent = movementComponent.replace(
     ':date',
     formatDate(movement.movement_date)
@@ -273,7 +316,15 @@ const buildMovementComponent = (movement) => {
     ':type',
     movement.movement_type
   );
-  movementComponent = movementComponent.replace(':amount', movement.amount);
+
+  movementComponent = movementComponent.replace(
+    ':currency',
+    currencyConfig[movement?.currency || 'ars']?.label
+  );
+
+  const formatedAmount = formatMoneyValue(movement.amount);
+
+  movementComponent = movementComponent.replace(':amount', formatedAmount);
   return movementComponent;
 };
 
@@ -292,10 +343,14 @@ const formatDate = (dateString) => {
 const renderMovements = (session) => {
   if (!session) return;
   const movements = getAllMovementsByUsername(session?.id);
-  movements.forEach((movement) => {
+
+  // lo invertimos para que se vea en orden cronologico
+  // y no tener que hacer un sort
+  for (let i = movements.length - 1; i >= 0; i--) {
+    const movement = movements[i];
     const movementComponent = buildMovementComponent(movement);
     if (movementsContainer) movementsContainer.innerHTML += movementComponent;
-  });
+  }
 };
 
 // Funciones de retiro
@@ -314,7 +369,7 @@ const setWithdrawListeners = () => {
 };
 
 // Realiza un retiro
-const makeWithdraw = (event) => {
+const makeWithdraw = async (event) => {
   event.preventDefault();
   const user = getSession()?.session_data.user;
   const withdrawAmount = withdrawInput.value;
@@ -322,7 +377,13 @@ const makeWithdraw = (event) => {
     withdrawErrorMessage.innerText = 'El monto debe ser mayor a 0';
     return;
   }
-  user.balance -= Number(withdrawAmount);
+
+  if (user.balance.ars < withdrawAmount) {
+    withdrawErrorMessage.innerText = 'No tienes suficiente saldo';
+    return;
+  }
+
+  user.balance.ars -= Number(withdrawAmount);
   updateItem(user, 'users');
   newMovement({
     username: user.id,
@@ -330,9 +391,17 @@ const makeWithdraw = (event) => {
     movement_type: 'retiro',
     amount: withdrawAmount,
   });
-  alert(`Se ha retirado $${withdrawAmount} de su cuenta`);
 
-  pathResolver('account/account.html');
+  const swalResult = await Swal.fire({
+    title: 'Retiro exitoso',
+    text: `Se ha retirado $${formatMoneyValue(withdrawAmount)} de su cuenta`,
+    icon: 'success',
+    confirmButtonText: 'Cool',
+  });
+
+  if (swalResult.isConfirmed || swalResult.isDismissed) {
+    pathResolver('account/account.html');
+  }
 };
 
 // Funciones de transferencia
@@ -341,7 +410,20 @@ let accountBalanceElement,
   transferAmountErrorMessage,
   transferRecipientInput,
   transferRecipientErrorMessage,
+  transferCurrencyBtn,
   transferBtn;
+
+let selectedCurrency = 'ars';
+const currencyConfig = {
+  ars: {
+    label: 'AR$',
+    minimum: 10,
+  },
+  usd: {
+    label: 'U$D',
+    minimum: 1,
+  },
+};
 
 // Obtiene referencias a los elementos del DOM relacionados con las transferencias
 const getTransfersListeners = () => {
@@ -353,29 +435,42 @@ const getTransfersListeners = () => {
   transferRecipientErrorMessage = document.getElementById(
     'transfer-recipient-error'
   );
+  transferCurrencyBtn = document.getElementById('transfer-currency-btn');
+  if (transferCurrencyBtn) {
+    const config = currencyConfig[selectedCurrency];
+    transferCurrencyBtn.innerHTML = `<i class="fa-solid fa-rotate"></i>${config.label}`;
+  }
 };
 
 // Establece los listeners para los botones de transferencia
 const setTransfersListeners = () => {
   transferBtn?.addEventListener('click', makeTransfer);
+
+  transferCurrencyBtn?.addEventListener('click', (event) => {
+    event.preventDefault();
+    selectedCurrency = selectedCurrency === 'ars' ? 'usd' : 'ars';
+    const config = currencyConfig[selectedCurrency];
+    transferCurrencyBtn.innerHTML = `<i class="fa-solid fa-rotate"></i>${config.label}`;
+  });
 };
 
 // Realiza una transferencia
-const makeTransfer = (event) => {
+const makeTransfer = async (event) => {
   event.preventDefault();
   const user = getSession()?.session_data.user;
+  const config = currencyConfig[selectedCurrency];
   const transferAmount = parseFloat(transferAmountInput.value);
   if (user.id === transferRecipientInput.value.toLowerCase()) {
     transferRecipientErrorMessage.innerText =
       'No puedes transferirte a ti mismo';
     return;
   }
-  if (transferAmount < 10 || isNaN(transferAmount)) {
+  if (transferAmount < config.minimum || isNaN(transferAmount)) {
     transferAmountErrorMessage.innerText =
       'El monto mínimo a transferir es de $10.00';
     return;
   }
-  if (user.balance < transferAmount) {
+  if (user.balance[selectedCurrency] < transferAmount) {
     transferAmountErrorMessage.innerText = 'No tienes suficiente saldo';
     return;
   } else {
@@ -397,14 +492,15 @@ const makeTransfer = (event) => {
   } else {
     transferRecipientErrorMessage.innerText = '';
   }
-  user.balance -= transferAmount;
-  recipientUser.balance += transferAmount;
+  user.balance[selectedCurrency] -= transferAmount;
+  recipientUser.balance[selectedCurrency] += transferAmount;
   updateItem(user, 'users');
   updateItem(recipientUser, 'users');
   newMovement({
     username: user.id,
     movement_date: new Date().getTime(),
-    movement_type: 'transferencia',
+    movement_type: `transferencia`,
+    currency: selectedCurrency,
     recipientUsername: recipientUser.id,
     amount: transferAmount,
   });
@@ -413,21 +509,55 @@ const makeTransfer = (event) => {
     username: recipientUser.id,
     movement_date: new Date().getTime(),
     movement_type: 'transferencia',
+    currency: selectedCurrency,
     senderUsername: user.id,
     amount: transferAmount,
   });
-  alert(`Se ha retirado $${transferAmount} de su cuenta`);
-  pathResolver('account/account.html');
+
+  const swalResult = await Swal.fire({
+    title: 'Transferencia exitosa',
+    text: `Se ha retirado $${formatMoneyValue(transferAmount)} de su cuenta`,
+    icon: 'success',
+    confirmButtonText: 'Cool',
+    backdrop: `
+    rgba(0,0,123,0.4)
+    url("/images/nyan-cat.gif")
+    left top
+    no-repeat
+  `,
+  });
+
+  if (swalResult.isConfirmed || swalResult.isDismissed) {
+    pathResolver('account/account.html');
+  }
+};
+
+// Obtiene el valor del dolar blue
+const getUsdToArs = async () => {
+  const response = await fetch('https://dolarapi.com/v1/dolares/blue');
+  const data = await response.json();
+  // no se cual elegir, asi que saco el promedio entre ambos valores
+  return (data.compra + data.venta) / 2;
+};
+
+const formatMoneyValue = (value) => {
+  if (isNaN(value)) return value;
+
+  return Number(value).toLocaleString('en-US', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
 };
 
 // Actualiza la pantalla con los datos del usuario
-const updateScreen = (userData) => {
+const updateScreen = async (userData) => {
   if (!userData) return;
-  const formattedBalance = `$${userData.balance.toLocaleString('en-US', {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  })}`;
-  if (accountBalanceElement) accountBalanceElement.innerText = formattedBalance;
+  const formattedBalance = `<li>$${formatMoneyValue(
+    userData.balance.ars
+  )} ARS </li>  
+  <li>$${formatMoneyValue(userData.balance.usd)} USD </li>
+  `;
+  if (accountBalanceElement) accountBalanceElement.innerHTML = formattedBalance;
 };
 
 // Funciones de depósito
@@ -442,28 +572,122 @@ const getDepositReferences = () => {
 
 // Establece los listeners para los botones de depósito
 const setDepositListeners = () => {
-  depositBtn?.addEventListener('click', makeDeposit);
+  // Realiza un depósito
+  depositBtn?.addEventListener('click', async (event) => {
+    event.preventDefault();
+    const user = getSession()?.session_data.user;
+    const depositAmount = depositInput.value;
+    if (depositAmount <= 0) {
+      depositErrorMessage.innerText = 'El monto a depositar debe ser mayor a 0';
+      return;
+    }
+    user.balance.ars += Number(depositAmount);
+    updateItem(user, 'users');
+    newMovement({
+      username: user.id,
+      movement_date: new Date().getTime(),
+      movement_type: 'deposito',
+      currency: 'ars',
+      amount: depositAmount,
+    });
+
+    const swalResult = await Swal.fire({
+      title: 'Deposito exitoso',
+      text: `Se ha depositado $${formatMoneyValue(depositAmount)} a su cuenta`,
+      icon: 'success',
+      confirmButtonText: 'Cool',
+    });
+
+    if (swalResult.isConfirmed || swalResult.isDismissed) {
+      pathResolver('account/account.html');
+    }
+  });
 };
 
-// Realiza un depósito
-const makeDeposit = (event) => {
-  event.preventDefault();
-  const user = getSession()?.session_data.user;
-  const depositAmount = depositInput.value;
-  if (depositAmount <= 0) {
-    depositErrorMessage.innerText = 'El monto a depositar debe ser mayor a 0';
-    return;
+let usdCheckoutInput,
+  usdCheckoutBtn,
+  usdCheckoutErrorMessage,
+  usdMarketValue,
+  usdToGet;
+// Obtiene referencias a los elementos del DOM relacionados con la compra de dolares
+const getUsdCheckoutReferences = () => {
+  usdCheckoutInput = document.getElementById('usd-checkout-amount');
+  usdCheckoutBtn = document.getElementById('usd-checkout-btn');
+  usdToGet = document.getElementById('usd-to-get');
+  usdMarketValue = document.getElementById('usd-market-value');
+  usdCheckoutErrorMessage = document.getElementById(
+    'usd-checkout-amount-error'
+  );
+};
+
+// Establece los listeners para los botones de compra de dolares
+const setUsdCheckoutListeners = async () => {
+  const usdValue = await getUsdToArs().catch(() => 1);
+  if (usdMarketValue) {
+    usdMarketValue.innerHTML = `$${formatMoneyValue(usdValue)}`;
   }
-  user.balance += Number(depositAmount);
-  updateItem(user, 'users');
-  newMovement({
-    username: user.id,
-    movement_date: new Date().getTime(),
-    movement_type: 'deposito',
-    amount: depositAmount,
+  const user = getSession()?.session_data.user;
+
+  usdCheckoutBtn?.addEventListener('click', async (event) => {
+    event.preventDefault();
+
+    // redondeamos a 2 decimales para evitar problemas con el dinero
+    const usdCheckoutAmount = (usdCheckoutInput.value / usdValue).toFixed(2);
+
+    if (usdCheckoutAmount <= 0) {
+      usdCheckoutErrorMessage.innerText = `El monto mínimo a comprar es de $${formatMoneyValue(
+        usdValue
+      )}`;
+      return;
+    }
+
+    if (usdCheckoutInput.value < usdValue) {
+      usdCheckoutErrorMessage.innerText = `El monto mínimo a comprar es de $${formatMoneyValue(
+        usdValue
+      )}`;
+      return;
+    }
+
+    user.balance.usd += Number(usdCheckoutAmount);
+    user.balance.ars -= Number(usdCheckoutInput.value);
+    updateItem(user, 'users');
+    newMovement({
+      username: user.id,
+      movement_date: new Date().getTime(),
+      movement_type: 'compra usd',
+      amount: usdCheckoutAmount,
+    });
+
+    const swalResult = await Swal.fire({
+      title: 'Compra exitosa',
+      text: `Se han depositado $${formatMoneyValue(usdCheckoutAmount)} ${
+        currencyConfig.usd.label
+      } a su cuenta`,
+      icon: 'success',
+      confirmButtonText: 'Cool',
+    });
+
+    if (swalResult.isConfirmed || swalResult.isDismissed) {
+      pathResolver('account/account.html');
+    }
   });
-  alert(`Se ha depositado $${depositAmount} a su cuenta`);
-  pathResolver('account/account.html');
+
+  usdCheckoutInput?.addEventListener('input', async (event) => {
+    event.preventDefault();
+    if (event.target.value > user.balance.ars) {
+      event.target.value = user.balance.ars;
+    }
+    const value = event.target.value;
+    if (value < usdValue) {
+      usdCheckoutErrorMessage.innerText = `El monto mínimo a comprar es de $${formatMoneyValue(
+        usdValue
+      )}`;
+      return;
+    }
+
+    usdCheckoutErrorMessage.innerText = '';
+    usdToGet.innerText = `USD: $${formatMoneyValue(value / usdValue)}`;
+  });
 };
 
 // Funciones del pie de página
@@ -479,6 +703,8 @@ const setFooterListeners = () => {
   logoutBtn?.addEventListener('click', logoutUser);
 };
 
+let usernameHtmlValue = document.getElementsByClassName('username');
+
 // Inicia el pie de página
 const initiateFooter = () => {
   getFooterReferences();
@@ -489,29 +715,16 @@ const initiateFooter = () => {
 const initiateDocument = () => {
   getDb();
   const session = validateSession();
+  if (session) {
+    for (let i = 0; i < usernameHtmlValue.length; i++) {
+      usernameHtmlValue[i].innerText = session.session_data.user.id;
+    }
+  }
   return session;
 };
 
-// resuelve la ruta relativa dentro de "pages"
-const pathResolver = (path) => {
-  console.log('MARTIN_LOG=> window', window.location);
-  console.log(
-    'MARTIN_LOG=> window.location.pathname',
-    window.location.pathname
-  );
-  console.log('MARTIN_LOG=> pathResolver -> path', path);
-
-  const pathList = window.location.pathname.split('/');
-  const pagesIndex = pathList.indexOf('pages');
-  path.split('/').forEach((pathPart, index) => {
-    pathList[pagesIndex + index + 1] = pathPart;
-  });
-  console.log('MARTIN_LOG=> pathResolver -> pathList', pathList.join('/'));
-  window.location.pathname = pathList.join('/');
-};
-
 // Listeners para el evento DOMContentLoaded
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
   const session = initiateDocument();
   getAuthElementReferences();
   setAuthListeners();
@@ -521,8 +734,10 @@ document.addEventListener('DOMContentLoaded', () => {
   setTransfersListeners();
   getDepositReferences();
   setDepositListeners();
+  getUsdCheckoutReferences();
+  setUsdCheckoutListeners();
   getMovementReferences();
   renderMovements(session?.session_data?.user);
   initiateFooter();
-  updateScreen(session?.session_data?.user);
+  await updateScreen(session?.session_data?.user);
 });
